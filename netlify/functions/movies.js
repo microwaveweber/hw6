986b4e2bec5e33ade28526f37153cf94635c6e37
Weber Lu <microwaveweber@gmail.com>
2021-05-13 21:10:25 +0800

    first commit

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -19,29 +19,51 @@ exports.handler = async function(event) {
   console.log(moviesFromCsv)
 
   // 🔥 hw6: your recipe and code starts here!
+
+  // deifne the year and genre parameters
   let year = event.queryStringParameters.year
   let genre = event.queryStringParameters.genre
-  
+
+  // create the two arrays to be returned by API
+  let movies = []
+  let numResults =0
+  // loop through movie data
+  for (let i = 0 ; i < moviesFromCsv.length ; i++) {
+    
+    // store each item to the array
+    let movieObject = moviesFromCsv[i]
+    
+    // skip items wtihout genre or run time data
+    if (movieObject.genres != `\\N` && movieObject.runtimeMinutes != `\\N`) {
+      // store results only if genre and start year equal to the specified values    
+      if (movieObject.genres.includes(genre) == true && movieObject.startYear.includes(year)==true) {
+      let movie = {
+        title: movieObject.primaryTitle,
+        year: movieObject.startYear,
+        genre: movieObject.genres
+      }
+      // push the result into the movies array
+      movies.push(movie)
+      // count numResults
+      numResults = numResults +1 
+
+    }
+   }
+  }
+  // return error message if the user does not specify year or genre
   if (year == undefined || genre == undefined) {
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Nope!` // a string of data
+      body: `Please enter year and genre` // a string of data
     }
   }
+  // return the result as a string
   else {
-    let returnValue = {
-      numResults: 0,
-      movies: []
-    }
-
-    for (let i=0; i < moviesFromCsv.length; i++) {
-
-    }
-
-    // a lambda function returns a status code and a string of data
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Hello from the back-end!` // a string of data
+      body: JSON.stringify({numResults, movies}) // a string of data
+      
     }
+
   }
 }
